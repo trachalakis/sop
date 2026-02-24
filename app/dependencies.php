84c20c09e4e1a8fdc8e170ea603ff2d1a583fc4b
Giannis Trachalakis <giannis@trachalakis.gr>
2026-02-24 09:16:42 +0200

    bump doctrine version

--- app/dependencies.php
+++ app/dependencies.php
@@ -47,11 +47,11 @@ use Domain\Repositories\SupplyGroupsRepositoryInterface;
 use Domain\Repositories\SuppliersRepositoryInterface;
 use DI\ContainerBuilder;
 use Psr\Container\ContainerInterface;
-use Doctrine\ORM\EntityManagerInterface;
 use Slim\Views\Twig;
-use Symfony\Component\Mailer\Transport;
-use Symfony\Component\Mailer\Mailer;
 use Application\Settings\Settings;
+use Doctrine\DBAL\DriverManager;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMSetup;
 
 return function (ContainerBuilder $containerBuilder) {
     $containerBuilder->addDefinitions([
@@ -113,10 +113,10 @@ return function (ContainerBuilder $containerBuilder) {
                 Twig::class
             );
         },*/
-        EntityManagerInterface::class => function (ContainerInterface $c) {
-            $settings = $c->get('settings')['db'];
+        EntityManager::class => function (ContainerInterface $c) {
+            $dbSettings = $c->get('settings')['db'];
            
-            return Doctrine\ORM\EntityManager::create(
+            /*return Doctrine\ORM\EntityManager::create(
                 [
                     'driver' => 'pdo_pgsql',
                     'host' => $settings['host'],
@@ -126,126 +126,142 @@ return function (ContainerBuilder $containerBuilder) {
                 ],
                 //TODO
                 Doctrine\ORM\Tools\Setup::createAnnotationMetadataConfiguration([__DIR__ . '/../src/Domain/Entities'], $isDevMode = true)
+            );*/
+
+            $config = ORMSetup::createAttributeMetadataConfig(
+                paths: [__DIR__ . '/../src/Domain/Entities'],
+                isDevMode: true,
             );
+            $config->enableNativeLazyObjects(true);
+            
+            $connection = DriverManager::getConnection([
+                'driver' => 'pdo_pgsql',
+                'host' => $dbSettings['host'],
+                'user' => $dbSettings['username'],
+                'password' => $dbSettings['password'],
+                'dbname' => $dbSettings['databaseName'],
+            ], $config);
+            
+            return new EntityManager($connection, $config);
         },
         LanguagesRepositoryInterface::class => function (ContainerInterface $c) {
-            $em = $c->get(EntityManagerInterface::class);
+            $em = $c->get(EntityManager::class);
 
             return $em->getRepository(Language::class);
         },
         MenusRepositoryInterface::class => function (ContainerInterface $c) {
-            $em = $c->get(EntityManagerInterface::class);
+            $em = $c->get(EntityManager::class);
 
             return $em->getRepository(Menu::class);
         },
         MenuSectionsRepositoryInterface::class => function (ContainerInterface $c) {
-            $em = $c->get(EntityManagerInterface::class);
+            $em = $c->get(EntityManager::class);
 
             return $em->getRepository(MenuSection::class);
         },
         MenuItemsRepositoryInterface::class => function (ContainerInterface $c) {
-            $em = $c->get(EntityManagerInterface::class);
+            $em = $c->get(EntityManager::class);
 
             return $em->getRepository(MenuItem::class);
         },
         MenuItemExtrasRepositoryInterface::class => function (ContainerInterface $c) {
-            $em = $c->get(EntityManagerInterface::class);
+            $em = $c->get(EntityManager::class);
 
             return $em->getRepository(MenuItemExtra::class);
         },
         OrdersRepositoryInterface::class => function (ContainerInterface $c) {
-            $em = $c->get(EntityManagerInterface::class);
+            $em = $c->get(EntityManager::class);
 
             return $em->getRepository(Order::class);
         },
         OrderEntriesRepositoryInterface::class => function (ContainerInterface $c) {
-            $em = $c->get(EntityManagerInterface::class);
+            $em = $c->get(EntityManager::class);
 
             return $em->getRepository(OrderEntry::class);
         },
         OrderEntryCancellationsRepositoryInterface::class => function (ContainerInterface $c) {
-            $em = $c->get(EntityManagerInterface::class);
+            $em = $c->get(EntityManager::class);
 
             return $em->getRepository(OrderEntryCancellation::class);
         },
         OrderEntryGroupsRepositoryInterface::class => function (ContainerInterface $c) {
-            $em = $c->get(EntityManagerInterface::class);
+            $em = $c->get(EntityManager::class);
 
             return $em->getRepository(OrderEntryGroup::class);
         },
         StationsRepositoryInterface::class => function (ContainerInterface $c) {
-            $em = $c->get(EntityManagerInterface::class);
+            $em = $c->get(EntityManager::class);
 
             return $em->getRepository(Station::class);
         },
         TablesRepositoryInterface::class => function (ContainerInterface $c) {
-            $em = $c->get(EntityManagerInterface::class);
+            $em = $c->get(EntityManager::class);
 
             return $em->getRepository(Table::class);
         },
         ReservationsRepositoryInterface::class => function (ContainerInterface $c) {
-            $em = $c->get(EntityManagerInterface::class);
+            $em = $c->get(EntityManager::class);
 
             return $em->getRepository(Reservation::class);
         },
         UsersRepositoryInterface::class => function (ContainerInterface $c) {
-            $em = $c->get(EntityManagerInterface::class);
+            $em = $c->get(EntityManager::class);
 
             return $em->getRepository(User::class);
         },
         ScansRepositoryInterface::class => function (ContainerInterface $c) {
-            $em = $c->get(EntityManagerInterface::class);
+            $em = $c->get(EntityManager::class);
 
             return $em->getRepository(Scan::class);
         },
         SuppliesRepositoryInterface::class => function (ContainerInterface $c) {
-            $em = $c->get(EntityManagerInterface::class);
+            $em = $c->get(EntityManager::class);
 
             return $em->getRepository(Supply::class);
         },
         SupplyGroupsRepositoryInterface::class => function (ContainerInterface $c) {
-            $em = $c->get(EntityManagerInterface::class);
+            $em = $c->get(EntityManager::class);
 
             return $em->getRepository(SupplyGroup::class);
         }, 
         SuppliersRepositoryInterface::class => function (ContainerInterface $c) {
-            $em = $c->get(EntityManagerInterface::class);
+            $em = $c->get(EntityManager::class);
 
             return $em->getRepository(Supplier::class);
         },
         /*'Domain\Repositories\SuppliesRepositoryInterface' => function (ContainerInterface $c) {
-            $em = $c->get(EntityManagerInterface::class);
+            $em = $c->get(EntityManager::class);
 
             return $em->getRepository(Supply::class);
         },
         'Domain\Repositories\InvoicesRepositoryInterface' => function (ContainerInterface $c) {
-            $em = $c->get(EntityManagerInterface::class);
+            $em = $c->get(EntityManager::class);
 
             return $em->getRepository(Invoice::class);
         },*/
         RecipesRepositoryInterface::class => function (ContainerInterface $c) {
-            $em = $c->get(EntityManagerInterface::class);
+            $em = $c->get(EntityManager::class);
 
             return $em->getRepository(Recipe::class);
         },
         /*'Domain\Repositories\PoStringsRepositoryInterface' => function (ContainerInterface $c) {
-            $em = $c->get(EntityManagerInterface::class);
+            $em = $c->get(EntityManager::class);
 
             return $em->getRepository(PoString::class);
         },*/
         UserPermissionsRepositoryInterface::class => function (ContainerInterface $c) {
-            $em = $c->get(EntityManagerInterface::class);
+            $em = $c->get(EntityManager::class);
 
             return $em->getRepository(UserPermission::class);
         },
         /*
         'Domain\Repositories\SupplyGroupsRepositoryInterface' => function (ContainerInterface $c) {
-            $em = $c->get(EntityManagerInterface::class);
+            $em = $c->get(EntityManager::class);
 
             return $em->getRepository(SupplyGroup::class);
         },
         'Domain\Repositories\ShoppingListsRepositoryInterface' => function (ContainerInterface $c) {
-            $em = $c->get(EntityManagerInterface::class);
+            $em = $c->get(EntityManager::class);
 
             return $em->getRepository(ShoppingList::class);
         }*/
