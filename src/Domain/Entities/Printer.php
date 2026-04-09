6c075d26fbe6bc89445774dba3df95e54dfd569e
Giannis Trachalakis <giannis@trachalakis.gr>
2026-04-09 22:01:46 +0300

    feat: add isUtilityPrinter field to Printer entity

--- src/Domain/Entities/Printer.php
+++ src/Domain/Entities/Printer.php
@@ -28,9 +28,12 @@ class Printer
 
     #[ORM\Column(type: 'string', name: 'printer_address')]
     private ?string $printerAddress;
-    
+
     #[ORM\Column(type: 'string', enumType: PrinterType::class, name: 'printer_type')]
     private PrinterType $printerType;
+
+    #[ORM\Column(type: 'boolean', name: 'is_utility_printer')]
+    private bool $isUtilityPrinter = false;
 
     public function getId(): int
     {
@@ -39,7 +42,7 @@ class Printer
 
     public function getIsReceiptPrinter(): bool
     {
-    	return $this->isReceiptPrinter;
+        return $this->isReceiptPrinter;
     }
 
     public function getIsActive(): bool
@@ -47,7 +50,7 @@ class Printer
         return $this->isActive;
     }
 
-	public function getName(): string
+    public function getName(): string
     {
         return $this->name;
     }
@@ -57,19 +60,24 @@ class Printer
         return $this->printerType->value;
     }
 
-	public function setName(string $name): void
-    {
-        $this->name = $name;
-    }
-
     public function getPrinterAddress(): ?string
     {
         return $this->printerAddress;
     }
 
+    public function getIsUtilityPrinter(): bool
+    {
+        return $this->isUtilityPrinter;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
     public function setHasReceiptPrinter(bool $hasReceiptPrinter): void
     {
-    	$this->hasReceiptPrinter = $hasReceiptPrinter;
+        $this->hasReceiptPrinter = $hasReceiptPrinter;
     }
 
     public function setIsActive(bool $isActive): void
@@ -77,7 +85,7 @@ class Printer
         $this->isActive = $isActive;
     }
 
-	public function setPrinterAddress(?string $printerAddress): void
+    public function setPrinterAddress(?string $printerAddress): void
     {
         $this->printerAddress = $printerAddress;
     }
@@ -85,5 +93,10 @@ class Printer
     public function setPrinterType(PrinterType $printerType): void
     {
         $this->printerType = $printerType;
+    }
+
+    public function setIsUtilityPrinter(bool $isUtilityPrinter): void
+    {
+        $this->isUtilityPrinter = $isUtilityPrinter;
     }
 }
