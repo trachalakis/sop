e4afcc5651d9d09e3d0ca3320039e227134c9185
Giannis Trachalakis <giannis@trachalakis.gr>
2026-04-17 00:10:59 +0300

    feat: add update supplier action and template

---
+++ src/Application/Actions/Admin/UpdateSupplier.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Actions\Admin;
+
+use Domain\Repositories\SuppliersRepository;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Views\Twig;
+
+final class UpdateSupplier
+{
+    public function __construct(
+        private SuppliersRepository $suppliersRepository,
+        private Twig $twig
+    ) {}
+
+    public function __invoke(Request $request, Response $response): Response
+    {
+        $supplier = $this->suppliersRepository->find($request->getQueryParams()['id']);
+
+        if ($request->getMethod() === 'POST') {
+            $data = $request->getParsedBody();
+            $supplier->setName($data['name']);
+            $supplier->setTelephone($data['telephone'] !== '' ? $data['telephone'] : null);
+            $this->suppliersRepository->persist($supplier);
+            return $response->withHeader('Location', '/admin/suppliers')->withStatus(302);
+        }
+
+        return $this->twig->render($response, 'admin/update_supplier.twig', [
+            'supplier' => $supplier,
+        ]);
+    }
+}
