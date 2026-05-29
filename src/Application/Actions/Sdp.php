c74862bcbf89f9e00372664d2a7a937ff04221c2
Giannis Trachalakis <giannis@trachalakis.gr>
2026-05-29 17:09:12 +0300

    fix to supress error message in logs

--- src/Application/Actions/Sdp.php
+++ src/Application/Actions/Sdp.php
@@ -22,46 +22,48 @@ final class Sdp
 	{
         $requestParams = $request->getParsedBody();
 
-        if ($requestParams['ConnectionType'] == 'SetResponse') {
-            $xml = simplexml_load_string($requestParams['ResponseFile']);
- 
-            if ($xml->ePOSPrint->PrintResponse->response['success'] == 'true') {
-                $printJob = $this->printJobsRepository->find(
-                    (int)$xml->ePOSPrint->Parameter->printjobid
-                );
-                $printJob->setStatus(PrintJobStatus::completed);
-            
-                $this->printJobsRepository->persist($printJob);
+        if (isset($requestParams['ConnectionType'])) {
+            if ($requestParams['ConnectionType'] == 'SetResponse') {
+                $xml = simplexml_load_string($requestParams['ResponseFile']);
+    
+                if ($xml->ePOSPrint->PrintResponse->response['success'] == 'true') {
+                    $printJob = $this->printJobsRepository->find(
+                        (int)$xml->ePOSPrint->Parameter->printjobid
+                    );
+                    $printJob->setStatus(PrintJobStatus::completed);
+                
+                    $this->printJobsRepository->persist($printJob);
+                }
+
+                $response->getBody()->write('');
             }
 
-            $response->getBody()->write('');
-        }
+            if ($requestParams['ConnectionType'] == 'GetRequest') {
+                $printJob = $this->printJobsRepository->findOneBy([
+                    'printer' => $requestParams['ID'],
+                    'status' => PrintJobStatus::pending
+                ]);
 
-        if ($requestParams['ConnectionType'] == 'GetRequest') {
-            $printJob = $this->printJobsRepository->findOneBy([
-                'printer' => $requestParams['ID'],
-                'status' => PrintJobStatus::pending
-            ]);
+                if ($printJob != null) {
+                    $response->getBody()->write(
+                        '<?xml version="1.0" encoding="utf-8"?>
+                            <PrintRequestInfo Version="2.00">
 
-            if ($printJob != null) {
-                $response->getBody()->write(
-                    '<?xml version="1.0" encoding="utf-8"?>
-                        <PrintRequestInfo Version="2.00">
+                            <ePOSPrint>
+                                <Parameter>
+                                <devid>local_printer</devid>
+                                <timeout>10000</timeout>
+                                <printjobid>' . $printJob->getId() . '</printjobid>
+                                </Parameter>
+                                <PrintData>' . $printJob->getXml() . '</PrintData>
+                            </ePOSPrint>
 
-                        <ePOSPrint>
-                            <Parameter>
-                            <devid>local_printer</devid>
-                            <timeout>10000</timeout>
-                            <printjobid>' . $printJob->getId() . '</printjobid>
-                            </Parameter>
-                            <PrintData>' . $printJob->getXml() . '</PrintData>
-                        </ePOSPrint>
-
-                        </PrintRequestInfo>'
-                );
-               
-            } else {
-                $response->getBody()->write('');
+                            </PrintRequestInfo>'
+                    );
+                
+                } else {
+                    $response->getBody()->write('');
+                }
             }
         }
         
